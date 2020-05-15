a948dc052ea4b31ad34e8119dbb052688724c85a
Nicky Chua <nickychuaharvard@gmail.com>
2020-05-15 17:19:28 -0400

    This is the first work using Git and Github!

---
+++ HappyStudy.R
@@ -0,0 +1,4 @@
+Everyday is a happy day!
+Devote your heart to study and meaningful things!
+Be confident that you will be successful!
+Be confident that you will have a happy family here in Canada!
